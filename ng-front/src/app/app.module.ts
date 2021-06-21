3de315090a3a64c93525f8abaa42e243e7b0563c
Dan4ik105 <60137868+Dan4ik105@users.noreply.github.com>
2021-06-21 13:29:12 +0300

    add image and fonts

--- ng-front/src/app/app.module.ts
+++ ng-front/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageModule } from './main-page/main-page.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
