37641c569f76e0b5d3e43ef10b045ba5e6977651
Dan4ik105 <60137868+Dan4ik105@users.noreply.github.com>
2021-06-21 15:36:46 +0300

    refactor:[MAINPAGE-1] add first-block

--- ng-front/src/app/main-page/main-page.module.ts
+++ ng-front/src/app/main-page/main-page.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
+import { FirstBlockComponent } from './first-block/first-block.component';
 
 @NgModule({
-  declarations: []
+  declarations: [
+    FirstBlockComponent
+  ],
+  exports: [
+    FirstBlockComponent
+  ]
 })
 
 export class MainPageModule{
